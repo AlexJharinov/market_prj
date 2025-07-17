227fb7b9931bc77f135de53cbf7afd1ee094dc27
Alex <magwai1@yandex.ru>
2025-07-17 17:06:42 +0400

    Создано новое приложение для блога, создана модель блоговой записи согласно тз.

---
+++ blog_store/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    content = models.TextField(verbose_name="Содержимое")
+    preview = preview = models.ImageField(upload_to='blog/previews/', null=True, blank=True, verbose_name="Превью")
+    creation_at = models.DateField(auto_now_add=True)
+    publication_sign = models.BooleanField(default=False, verbose_name="Опубликовано")
+    number_views = models.PositiveIntegerField(default=0, verbose_name="Количество просмотров")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "<Блоговая запись>"
+        verbose_name_plural = "Блоговые записи"
+        ordering = ["-creation_at"]
+
+
