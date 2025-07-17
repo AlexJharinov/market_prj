d455a332576a4573a15a408a9e7ca1f2dd0953fe
Alex <magwai1@yandex.ru>
2025-07-17 20:17:57 +0400

    Добавлен контроллер просмотра основной страницы Добавлена home.html

--- blog_store/views.py
+++ blog_store/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import DetailView
+
+from blog_store.models import Blog
+
+
+class BlogDetailView(DetailView):
+    model = Blog
